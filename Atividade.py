84c92248a3591478fb90e1a5a5e3b39d27fb074e
Kethelyn Skripnik <kethelynjhoanny@hotmail.com>
2024-01-24 18:16:46 -0300

    arrumando banco de dados que nao deu certo e documentaçao

--- Atividade.py
+++ Atividade.py
@@ -1,8 +1,12 @@
-import sqlite3
 from plyer import notification
 from datetime import datetime
 import requests
 import pandas as pd
+from pyspark.sql import SparkSession
+
+url_bancos = "https://brasilapi.com.br/api/banks/v1"
+response = None
+success_api_con = False
 
 def alerta(nivel, base, etapa):
     
@@ -20,11 +24,18 @@ def alerta(nivel, base, etapa):
         message = mensagem,
     )
 
-
-url_bancos = "https://brasilapi.com.br/api/banks/v1"
 try:
     response = requests.get(url_bancos)
     response.raise_for_status() 
+    success_api_con = True
+
+except Exception as e:  
+    alerta(3, "bancos", "extração")
+    print(f"Erro na extração dos bancos: {e}")
+
+
+def dados_api():
+    
     bancos_data = response.json()        
     
     df =  pd.DataFrame(bancos_data)
@@ -51,26 +62,96 @@ try:
 
     df2_stacked = df2.stack() # transformando colunas em index da tabela
 
-    print(df2_stacked)  
+    print(df2_stacked)     
 
-# Criando o banco de dados SQLite
-    conn = sqlite3.connect('dados_bancos.db')
+    # Criando uma sessão Spark 
 
-    # Inserindo DataFrame no banco de dados
-    df2.to_sql('bancos', conn, if_exists='replace', index=False)    
+    pandas_df = df2
 
-    # Executando a consulta SQL para visualizar dados
-    consulta = conn.execute('SELECT * FROM bancos')
-    resultados = consulta.fetchall()
+    spark = SparkSession.builder.appName('PandasToSparkDF').getOrCreate()
+
+    # Convertendo um Pandas Dataframe em Spark Dataframe
+    df_spark = spark.createDataFrame(pandas_df)
+
+    # Imprimindo o dataframe Spark
+    # display(df_spark)
+
+    # Criando uma tabela temporária a partir do dataframe Pandas convertido em Spark dataframe
+    df_spark.createOrReplaceTempView("TB_TEMP_BANKS_BRASIL_API")
+
+    # Validações das colunas do dataframe spark mediante consultas em mysql
+
+    # Validando o campo fullName - Retorna apenas as linhas que no campo nome contêm apenas caracteres, espaços e acentos
+    df = spark.sql("""
+
+        SELECT *
+
+        FROM  TB_TEMP_BANKS_BRASIL_API AS tb
+
+    WHERE tb.fullName REGEXP '^[a-zA-ZÀ-ú ]+$'; 
     
-    print("\nDados no banco de dados:")
-    for resultado in resultados:
-        print(resultado)  
-    
+    """)
 
-except Exception as e:  
-    alerta(3, "bancos", "extração")
-    print(f"Erro na extração dos bancos: {e}")
+    print("Quantidade de tuplas que contêm apenas caracteres, espaços e acentos no campo fullName:",df.count())
+    display(df)
 
-finally:
-    conn.close()
+
+    # Validando o campo name - Retorna apenas as linhas que no campo nome que possuem número no nome
+    df = spark.sql("""
+
+        SELECT *
+
+        FROM  TB_TEMP_BANKS_BRASIL_API AS tb
+        WHERE tb.name NOT REGEXP '^[^0-9]+$'; -- CAMPO NOME QUE CONTÊM NÚMEROS   
+        
+    """)
+
+    print("Quantidade de tuplas que contêm números no campo name:",df.count())
+    display(df)
+
+
+
+    # Validando o campo ispb - Retorna apenas as linhas que no campo ispb está contêm no máximo 8 digitos inteiros
+    df = spark.sql("""
+
+        SELECT *
+
+        FROM  TB_TEMP_BANKS_BRASIL_API AS tb   
+
+    WHERE tb.ispb  BETWEEN 0 AND 99999999 
+        
+    """)
+
+    print("Quantidade de tuplas VÁLIDAS no campo ispb:",df.count())
+    display(df)
+
+    # Validando o campo Code - achando registros com mesmo código
+    df = spark.sql("""
+                
+        SELECT 
+        
+        tb1.*,
+        COD_REPETIDO.CONTADOR
+
+        FROM  TB_TEMP_BANKS_BRASIL_API AS tb1,
+
+        (SELECT   
+
+            tb.Code,  
+            COUNT(*) AS CONTADOR
+
+        FROM TB_TEMP_BANKS_BRASIL_API AS tb
+
+        GROUP BY 1
+        
+        HAVING CONTADOR > 1) AS COD_REPETIDO   
+
+        WHERE COD_REPETIDO.Code = tb1.Code   
+        
+    """)
+
+    print("Quantidade de tuplas que possuem o mesmo Code:",df.count())
+    display(df)
+
+if success_api_con == True:
+    dados_api()
